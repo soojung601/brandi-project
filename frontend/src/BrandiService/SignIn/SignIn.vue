75ba3be80952318e644397e6f90ab5ccd6da1f86
KIM <rudqo14@gmail.com>
2020-08-22 01:40:17 +0900

    Add : 로그인 페이지 레이아웃 완료 및 reset.css 작성

---
+++ frontend/src/BrandiService/SignIn/SignIn.vue
@@ -0,0 +1,121 @@
+<template>
+  <section class="main">
+    <main class="article">
+      <h1 class="title">오늘 사면 내일 도착!</h1>
+      <h2 class="subTitle">무료배송으로 내일 받는 브랜디 LOGIN</h2>
+      <div class="loginContainer">
+        <input class="loginInput" placeholder="아이디 입력" />
+        <input class="loginInput" placeholder="비밀번호 입력" />
+        <a class="loginBtn">로그인</a>
+        <a class="JoinBtn">회원가입</a>
+        <div class="loginFind">
+          <span class="findId">아이디 찾기</span>
+          <span class="border">|</span>
+          <span class="findPw">비밀번호 찾기</span>
+        </div>
+        <h3 class="socialTitle">간편 로그인 / 가입</h3>
+        <button class="googleLogin">Google 계정으로 계속하기</button>
+      </div>
+    </main>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.main {
+  display: flex;
+
+  .article {
+    height: 100%;
+    width: 1300px;
+    margin: 287px auto 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .title {
+      font-size: 34px;
+      font-weight: bold;
+      color: #ff204b;
+      font-family: "Spoqa Han Sans", Sans-serif;
+    }
+
+    .subTitle {
+      margin-top: 20px;
+      font-size: 32px;
+      font-weight: 100;
+      font-family: "Spoqa Han Sans", Sans-serif;
+    }
+
+    .loginContainer {
+      width: 600px;
+      margin-top: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      .loginInput {
+        height: 50px;
+        border: 1px solid lightgray;
+        border-radius: 3px;
+        margin-top: 10px;
+        padding: 10px;
+        outline: none;
+        font-size: 14px;
+        font-weight: 500;
+        color: #333333;
+      }
+
+      .loginBtn {
+        height: 50px;
+        line-height: 50px;
+        margin-top: 14px;
+        font-size: 13px;
+        text-align: center;
+        border-radius: 4px;
+        background-color: black;
+        color: white;
+        cursor: pointer;
+      }
+
+      .joinBtn {
+        height: 50px;
+        line-height: 50px;
+        margin-top: 4px;
+        font-size: 13px;
+        text-align: center;
+        border: 1px solid black;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+
+      .loginFind {
+        text-align: right;
+        padding: 17px 0;
+        font-size: 13px;
+        color: #666;
+        border-bottom: 1px solid #ccc;
+
+        .findId,
+        .findPw {
+          cursor: pointer;
+        }
+      }
+
+      .socialTitle {
+        margin: 15px;
+        padding-top: 20px;
+        text-align: center;
+        font-weight: bold;
+        font-size: 16px;
+      }
+
+      .googleLogin {
+        margin-bottom: 50px;
+      }
+    }
+  }
+}
+</style>
